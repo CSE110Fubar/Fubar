403bbea220054643294ca82af336b4ff519317c1
rosekows <rosekows@ucsd.edu>
2017-11-16 20:52:12 -0800

    Updated the format of the SearchResultPage

--- src/assets/js/pages/SearchResultPage/index.js
+++ src/assets/js/pages/SearchResultPage/index.js
@@ -4,6 +4,7 @@ import * as Api from '~/data/Api';
 import Hero from '~/components/Hero';
 import CauseCard from '~/components/CauseCard';
 import PublicFigureCard from '~/components/PublicFigureCard';
+import PetitionCard from '~/components/PetitionCard';
 
 export default class SearchResultPage extends React.Component {
   constructor(props) {
@@ -11,28 +12,54 @@ export default class SearchResultPage extends React.Component {
 
     // Placeholder state
     this.state = {
-      results: {}
+      causes: {},
+      publicFigures: {},
+      petitions: {}
     };
   }
 
   componentWillMount() {
+
+    let causes = Api.getCausesRef();
+    causes.once('value').then((snapshot) =>
+      this.setState({causes: snapshot.val()})
+    );
+
     // Load data from API here, store in state
-    let results = Api.getPublicFigureResults();
-    results.once('value').then((snapshot) =>
-      this.setState({results: snapshot.val()})
+    let publicFigures = Api.getPublicFigureResults();
+    publicFigures.once('value').then((snapshot) =>
+      this.setState({publicFigures: snapshot.val()})
+    );
+
+    let petitions = Api.getPetitionsRef();
+    petitions.once('value').then((snapshot) =>
+      this.setState({petitions: snapshot.val()})
     );
   }
 
 	render() {
-    let {results} = this.state;
+    let {causes, publicFigures, petitions} = this.state;
 
-		return (<div className="results-page">
+		return (<div className="search-results-page">
       <Hero />
       <div className="container">
-        <h1 className="results-page__header">Results</h1>
+        <h1 className="search-results-page__header">Results</h1>
+        <h2>Causes</h2>
         <div className="row">
-          {Object.keys(results).map((publicFigureId) => 
-            <PublicFigureCard publicFigure={results[publicFigureId]} publicFigureId={publicFigureId} key={publicFigureId} />
+          {Object.keys(causes).map((causeId) =>
+            <CauseCard cause={causes[causeId]} causeId={causeId} key={causeId} />
+          )}
+        </div>
+        <h2>Public Figures</h2>
+        <div className="row">
+          {Object.keys(publicFigures).map((publicFigureId) => 
+            <PublicFigureCard publicFigure={publicFigures[publicFigureId]} publicFigureId={publicFigureId} key={publicFigureId} />
+          )}
+        </div>
+        <h2>Petitions</h2>
+        <div className="row">
+          {Object.keys(petitions).map((petitionId) => 
+            <PetitionCard petition={petitions[petitionId]} petitionId={petitionId} key={petitionId} />
           )}
         </div>
       </div>
