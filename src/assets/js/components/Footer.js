93a4de600426db3e34ed44ca66debe5d12e8804d
anooj <futur@ubuntu.ubuntu-domain>
2017-12-04 15:55:52 -0800

    Added search and petitions to footer

--- src/assets/js/components/Footer.js
+++ src/assets/js/components/Footer.js
@@ -12,15 +12,21 @@ export default class Footer extends React.Component {
         <div className="row">
           <div className="col-md-6 offset-md-3">
             <div className="row">
-              <div className="col-md-3 text-center text-md-left">
+              <div className="col-md-2 text-center text-md-left">
                 <Link to="/" className="footer__link">Home</Link>
-                  </div>
-                  <div className="col-md-6 text-center">
+              </div>
+              <div className="col-md-2 text-center text-md-right">
+                <Link to="/search" className="footer__link">Search</Link>
+              </div>
+                  <div className="col-md-4 text-center">
                     <img src="/img/face.png" className="hero__logo" />
                   </div>
-                  <div className="col-md-3 text-center text-md-right">
-                    <Link to="/about" className="footer__link">About</Link>
-                  </div>
+              <div className="col-md-2 text-center text-md-left">
+                <Link to="/petitions" className="footer__link">Petitions</Link>
+              </div>
+              <div className="col-md-2 text-center text-md-right">
+                <Link to="/settings" className="footer__link">Settings</Link>
+              </div>
             </div>
           </div>
         </div>
