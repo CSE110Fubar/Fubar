978121b87d03eb9401ccc99ef0b88ff7b6500ef1
jccchen103 <jac186@ucsd.edu>
2017-12-07 14:05:23 -0800

    fixed error for when not logged in users try to go to petitions page

--- src/assets/js/pages/PetitionsPage/index.js
+++ src/assets/js/pages/PetitionsPage/index.js
@@ -65,7 +65,7 @@ export default class PetitionsPage extends React.Component {
   };
 
 	render() {
-    let {petitions} = this.state;
+    let {petitions, user} = this.state;
     let {name, description, image} = this.state
 
 		return (<div className="search-result-page">
@@ -76,16 +76,17 @@ export default class PetitionsPage extends React.Component {
         <div className="row">
           <div className="col-md-10">
             <h1 className="petition-page__header">Active Petitions</h1>
+            {!user && <p>Log in to support or add a petition for a new Cause.</p>}
           </div>
           <div className="col-md-2 text-right">
-            <button type="button" data-toggle="modal" data-target="#petition_form" className="btn btn-outline-primary">
+            <button type="button" data-toggle="modal" data-target="#petition_form" className="btn btn-outline-primary"  disabled={!user}>
               Add Petition
             </button>
           </div>
         </div>
         <div>
           {Object.keys(petitions).map((petitionId) => 
-            <PetitionCard petition={petitions[petitionId]} petitionId={petitionId} currentUser={this.state.user.uid} deletePetition={this.deletePetition} key={petitionId} />
+            <PetitionCard petition={petitions[petitionId]} petitionId={petitionId} currentUser={user && this.state.user.uid} deletePetition={this.deletePetition} key={petitionId} />
           )}
         </div>
       </div>
